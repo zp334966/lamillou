e30d825e9c20b526306ffa4c588941e9fef0e1a6
zp334966 <zanka000@hotmail.com>
2017-05-04 14:17:50 +0200

    Update combined2.js

--- combined2.js
+++ combined2.js
@@ -962,7 +962,7 @@ if ($(window).width() > 992) {
 function productNames(full_name) {
   var categories = ["KOCYK","PODUSIA","ZESTAW","OTULACZ","BLANKET","KOC","WINGS","BAG","PILLOW","KOMPLET","POŚCIEL",
 		    "ORGANIZER","OCHRANIACZ", "POZYTYWKA","HORN","PRZEŚCIERADŁO","PACK","TORBA","PASEK","PRZEWIJAK",
-		    "SASZETKA","ŚPIWOREK","PAD","COVER","KURA","COMBO", "ZAWIESZKA", "ZABAWKA", "WOREK", "COLLECTION",
+		    "SASZETKA","ŚPIWOREK","PAD","COVER","KURA","COMBO", "ZAWIESZKA", "ZABAWKA", "WOREK",
                     "THERMO", "GRZECHOTKA","PLECAK","MAT","KAPELUSZ", "CHUSTKA", "APASZKA", "CZAPA", "HAT"];
   var category = "";
   var names = full_name.split('-');
