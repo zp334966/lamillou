4073e61a2123cc4257a5d7d49b22af2174ce90e2
zp334966 <zanka000@hotmail.com>
2017-05-04 19:53:15 +0200

    Update combined2.js

--- combined2.js
+++ combined2.js
@@ -439,8 +439,7 @@ $('.back-search').on('click', function(e) {
   
   /*Moodboard Rectangular image - resize
   *******************************************/
-  $( document ).ready(function() {
-	  
+$(window).on('load', function(){	  
     $('.item-narrow').each(function() {
       if ($(window).width() >= 580) {      
         var h = $('.col-md-3 .img-responsive').css("height");
@@ -455,9 +454,8 @@ $('.back-search').on('click', function(e) {
         } else {
           $('.item-narrow').css( "max-height", "unset");
       	}
-    });
-	  
-  });  
+    });	  
+});  
   
   /*Quick Search Form Animation
   *******************************************/
@@ -1055,7 +1053,7 @@ function updateCartColor() {
   }        
   return;
 };
-var intervalID = setInterval(updateCartColor, 200);
+var cartColorInterval = setInterval(updateCartColor, 200);
 
 /************** instasive *********/
 window.lightwidget||(window.lightwidget=function(){"use strict";var t=[],i=0,n=!1,e=!1,o=function(t){return e=t},d=function(t,i){t.contentWindow&&t.contentWindow.postMessage("sizing:"+i,window.location.protocol+"//lightwidget.com")},c=function(i){if(i.origin==window.location.protocol+"//lightwidget.com"||i.origin==window.location.protocol+"//instansive.com"){var n=i.data.split(":");try{"sizing"==n[0]&&void 0!=t[parseInt(n[2])]&&(t[parseInt(n[2])].style.height=n[1]+"px")}catch(e){}}},s=function(t){e&&console.log(t);var i=t.origin.replace(/^https?\:\/\//i,"");if("lightwidget.com"==i||"instansive.com"==i){var n=t.data.split(":");e&&console.log(n);try{if("sizing_iid"==n[0]){var o=n[2];void 0!=document.getElementById(o)?document.getElementById(o).style.height=n[1]+"px":(o=o.replace("instansive","lightwidget"),void 0!=document.getElementById(o)&&(document.getElementById(o).style.height=n[1]+"px"))}}catch(d){e&&console.log(d)}}},g=function(){window.addEventListener?(window.addEventListener("message",c,!1),window.addEventListener("message",s,!1)):(window.attachEvent("onmessage",c),window.attachEvent("onmessage",s))};return g(),{refresh:function(){if(n)for(var e=0;i>e;e++)d(t[e],e)},reload:function(){g()},debug:function(t){return o(t)}}}()),window.lightwidget.refresh();
