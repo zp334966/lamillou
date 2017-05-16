ca3a246afd53827d042ba9cf5c4547cb2bd3b98a
zp334966 <zanka000@hotmail.com>
2017-05-16 10:42:53 +0200

    Update combined2.js

--- combined2.js
+++ combined2.js
@@ -955,6 +955,9 @@ if ($(window).width() > 992) {
   if ($elements.length !== 0) Intense($elements);
 }
 
+/************** instasive *********/
+window.lightwidget||(window.lightwidget=function(){"use strict";var t=[],i=0,n=!1,e=!1,o=function(t){return e=t},d=function(t,i){t.contentWindow&&t.contentWindow.postMessage("sizing:"+i,window.location.protocol+"//lightwidget.com")},c=function(i){if(i.origin==window.location.protocol+"//lightwidget.com"||i.origin==window.location.protocol+"//instansive.com"){var n=i.data.split(":");try{"sizing"==n[0]&&void 0!=t[parseInt(n[2])]&&(t[parseInt(n[2])].style.height=n[1]+"px")}catch(e){}}},s=function(t){e&&console.log(t);var i=t.origin.replace(/^https?\:\/\//i,"");if("lightwidget.com"==i||"instansive.com"==i){var n=t.data.split(":");e&&console.log(n);try{if("sizing_iid"==n[0]){var o=n[2];void 0!=document.getElementById(o)?document.getElementById(o).style.height=n[1]+"px":(o=o.replace("instansive","lightwidget"),void 0!=document.getElementById(o)&&(document.getElementById(o).style.height=n[1]+"px"))}}catch(d){e&&console.log(d)}}},g=function(){window.addEventListener?(window.addEventListener("message",c,!1),window.addEventListener("message",s,!1)):(window.attachEvent("onmessage",c),window.attachEvent("onmessage",s))};return g(),{refresh:function(){if(n)for(var e=0;i>e;e++)d(t[e],e)},reload:function(){g()},debug:function(t){return o(t)}}}()),window.lightwidget.refresh();
+
 
 /*Name Product
 *************************************************/
@@ -1047,17 +1050,17 @@ $(document).ready(function(){
 });
 
 /*Unavaible products label
-*************************************************/
+*************************************************/ /*
 $(document).ready(function(){
   $('.unavaible-products div').each(function() {
     if (! $( '.unavaible-products a' ).length ){
       $('.unavaible-products').css( "display", "none" );
     }
   });
-});
+});*/
 
 /*Cart Color
-*************************************************/
+*************************************************/ /*
 function updateCartColor() {
   var c = $('span.shop_cart_simple').text();
   var cart = parseInt( c, 10);
@@ -1069,7 +1072,7 @@ function updateCartColor() {
     $('#lm-cart span').css("opacity", "0.2");
   }        
   return;
-};
+};*/
 
 
 /*Pagination
@@ -1126,8 +1129,3 @@ poz.addEventListener('ended', function(){
   pozaction();
 });
 */
-
-
-/************** instasive *********/
-window.lightwidget||(window.lightwidget=function(){"use strict";var t=[],i=0,n=!1,e=!1,o=function(t){return e=t},d=function(t,i){t.contentWindow&&t.contentWindow.postMessage("sizing:"+i,window.location.protocol+"//lightwidget.com")},c=function(i){if(i.origin==window.location.protocol+"//lightwidget.com"||i.origin==window.location.protocol+"//instansive.com"){var n=i.data.split(":");try{"sizing"==n[0]&&void 0!=t[parseInt(n[2])]&&(t[parseInt(n[2])].style.height=n[1]+"px")}catch(e){}}},s=function(t){e&&console.log(t);var i=t.origin.replace(/^https?\:\/\//i,"");if("lightwidget.com"==i||"instansive.com"==i){var n=t.data.split(":");e&&console.log(n);try{if("sizing_iid"==n[0]){var o=n[2];void 0!=document.getElementById(o)?document.getElementById(o).style.height=n[1]+"px":(o=o.replace("instansive","lightwidget"),void 0!=document.getElementById(o)&&(document.getElementById(o).style.height=n[1]+"px"))}}catch(d){e&&console.log(d)}}},g=function(){window.addEventListener?(window.addEventListener("message",c,!1),window.addEventListener("message",s,!1)):(window.attachEvent("onmessage",c),window.attachEvent("onmessage",s))};return g(),{refresh:function(){if(n)for(var e=0;i>e;e++)d(t[e],e)},reload:function(){g()},debug:function(t){return o(t)}}}()),window.lightwidget.refresh();
-
