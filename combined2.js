75995b821c6d3232d3f04b9215a9d6548658b4a2
zp334966 <zanka000@hotmail.com>
2017-06-13 10:56:05 +0200

    Create combined2.js

--- combined2.js
+++ combined2.js
@@ -989,7 +989,7 @@ var advertDesVisible = 'none';
 function photoDisplay() {
   $('.sp-slider').addClass("photo-view");
   $('#exit-photo-view').addClass("photo-view");   
-  $('sp-slider-photo-view').addClass("active"); 
+  $('.sp-slider-photo-view').addClass("active"); 
 };
 function photoHideLayout() {
   $('.sticky-wrapper').hide();
@@ -1011,7 +1011,7 @@ function photoView() {
 function HidePhotoView() {
   $('.sp-slider').removeClass("photo-view");
   $('#exit-photo-view').removeClass("photo-view");
-  $('sp-slider-photo-view').removeClass("active"); 
+  $('.sp-slider-photo-view').removeClass("active"); 
   $('.sticky-wrapper').show();
   $('.advertisement-mobile').css('display', advertMobVisible);
   $('.advertisement-desktop').css('display', advertDesVisible);
