2e179ef4fd15528d905142eb72514846c5d4ae45
zp334966 <zanka000@hotmail.com>
2017-06-13 12:45:33 +0200

    Create combined2.js

--- combined2.js
+++ combined2.js
@@ -1033,14 +1033,14 @@ if ( $('.sp-slider-photo-view').length ) {
 	$(window).load ( function () {
 	    var h = $( '.ms-view.ms-fade-view' ).height();
 	    var hWindow = $( window ).height(); 
-	    var h2 = $('.sp-slider photo-view').height() + 27;
+	    var h2 = $('.sp-slider.photo-view').height() + 27;
 	    $( '.sp-slider-photo-view' ).height(h);
 	    $( '.canvas-photo-view' ).height(hWindow - h2);
 	});  
 	$(window).on('resize', function(){
 	    var h = $( '.ms-view.ms-fade-view' ).height();
  	    var hWindow = $( window ).height(); 
-	    var h2 = $('.sp-slider photo-view').height() + 27;
+	    var h2 = $('.sp-slider.photo-view').height() + 27;
 	    $( '.sp-slider-photo-view' ).height(h);
 	    $( '.canvas-photo-view' ).height(hWindow - h2);
 	});
