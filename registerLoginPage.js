d650f1e8eae29e27a0871e60b533256bda3f4558
zp334966 <zanka000@hotmail.com>
2017-06-14 10:52:11 +0200

    Create registerLoginPage.js

--- registerLoginPage.js
+++ registerLoginPage.js
@@ -20,8 +20,8 @@ function cmsRegister () {
           }     
         });  
         $('.cms_register .cms_buttons input').each(function() {      
-          $(this)[0].nextSibling.attr('style','display:none !important');
-          $(this)[0].nextSibling.attr('style','display:none !important');
+          $(this)[0].nextSibling.remove;
+          $(this)[0].nextSibling.remove;
         });     
     });
   }    
