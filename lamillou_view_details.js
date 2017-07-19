400fb1b0cfa0fcc904cfd3d05156d48c2ed375ca
zp334966 <zanka000@hotmail.com>
2017-07-19 11:37:01 +0200

    Update lamillou_view_details.js

--- lamillou_view_details.js
+++ lamillou_view_details.js
@@ -1,3 +1,6 @@
+
+/* Relate products display
+/**********************************************/
 var items = document.getElementsByClassName("relate_product"); 
 var allProducts = false;
 if (items.length >= 4) {
@@ -42,3 +45,57 @@ $('.load-more-products').on("click", function(e) {
     };
 });
 
+$('.sidebar').hide();
+
+/* Menu Packing
+*************************************************/ 
+function activeMenuPacking(i) {
+  $('.menu-packing .menu-title .title').each(function(e) {
+       if (i == $(this).index()) {
+          $(this).addClass('active');
+       } else {
+          $(this).removeClass('active');
+       }
+  });
+  $('.menu-packing .content .packing-info').each(function(e) {
+       if (i == $(this).index()) {
+          $(this).addClass('active');
+       } else {
+          $(this).removeClass('active');
+       }
+  });
+};     
+$('.menu-packing .menu-title .title').on("click", function(e) {
+    var i = $(this).index();
+    activeMenuPacking(i);
+});
+
+
+/* Load more relate products
+*************************************************/ 
+var close = true;
+function learnMore() {
+  $('.product_right_tab .block-devider .tab-pane').css('visibility', 'hidden');
+  var hRight = $('.product_right_tab').height(); 
+  var hLeft = $('.sp-slider').height(); 
+  if ($(window).width() >= 992 && hRight > hLeft && close) {
+    $('#learn-more').show();
+    var hBlockDevider = $('.product_right_tab .block-devider .tab-pane').height();
+    hBlockDevider = hBlockDevider - (hRight - hLeft) - 60; 
+    $('.product_right_tab .block-devider .tab-pane').height(hBlockDevider);    
+  } else {
+    $('#learn-more').hide();
+    $('.product_right_tab .block-devider .tab-pane').height('auto');
+  }
+  $('.product_right_tab .block-devider .tab-pane').css('visibility', 'visible');
+};
+  
+if ( $('#learn-more').length ) {
+    $(window).load(learnMore);
+	$(window).on('resize', learnMore);
+};
+$('#learn-more').on("click", function(e) {
+    $('#learn-more').hide();
+    $('.product_right_tab .block-devider .tab-pane').height('auto');
+    close = false;
+});
