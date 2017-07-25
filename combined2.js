ccc8ba16d4e12c5d5e226b243617b30bdb385990
zp334966 <zanka000@hotmail.com>
2017-07-25 22:21:46 +0200

    Update combined2.js

--- combined2.js
+++ combined2.js
@@ -270,7 +270,7 @@ jQuery(document).ready(function($) {
 	
   $(window).scroll(function(event){
     var st = $(this).scrollTop();
-    if (st > lastScrollTop + 25){
+    if (st > lastScrollTop + 60){
       // downscroll code
       $("header").addClass("headroom--at-top").removeClass("stuck");
     } else {
