e22d15c5bf053c9bb21b4416326f2518d125ee38
zp334966 <zanka000@hotmail.com>
2017-07-26 22:03:45 +0200

    Update headroom.js

--- headroom.js
+++ headroom.js
@@ -127,11 +127,11 @@
     if ($(window).width() < 1201) return 0;
   }
   function countOffsetTop () {
-    if ( $(".advertisement-desktop").css('display') == 'none') {
-      return 30;
+    if ( $(".advertisement-desktop").css('display') == 'none') { return 0;
+      /*return 30;
     } else { 
       return 70;
-    } 
+    } */
   } 
   
   /**
