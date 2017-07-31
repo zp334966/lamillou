98ad02f7d223d96640ebbe55613d5af6e9698190
zp334966 <zanka000@hotmail.com>
2017-07-31 21:36:12 +0200

    Update registerLoginPage.js

--- registerLoginPage.js
+++ registerLoginPage.js
@@ -200,7 +200,7 @@ function orderLastStep () {
       hideInHeader();
     }
     if (! $( "#order_number_block .shipping_information_title" ).length ) {  
-      $( "#shop_order_payment" ).after("<div id='shop_order_payment_info'><p>" + 
+      $( ".buttons.panel" ).after("<div id='shop_order_payment_info'><p>" + 
         "Masz prawo do odstąpienia od umowy w terminie 14 dni. Koszty zwrotu zamówienia ponosi zamawiający.<br/>" +
         "<a href='/regulamin'>Przeczytaj więcej</a></p>" + 
         "<p>Złożenie zamówienia wiąże się z obowiązkiem zapłaty.</p></div>");
