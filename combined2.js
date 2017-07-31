346784bd79f5c37214c3719b419cd469e144c381
zp334966 <zanka000@hotmail.com>
2017-07-31 15:05:30 +0200

    Update combined2.js

--- combined2.js
+++ combined2.js
@@ -928,7 +928,20 @@ if ($(window).width() > 992) {
 /************** instasive *********/
 window.lightwidget||(window.lightwidget=function(){"use strict";var t=[],i=0,n=!1,e=!1,o=function(t){return e=t},d=function(t,i){t.contentWindow&&t.contentWindow.postMessage("sizing:"+i,window.location.protocol+"//lightwidget.com")},c=function(i){if(i.origin==window.location.protocol+"//lightwidget.com"||i.origin==window.location.protocol+"//instansive.com"){var n=i.data.split(":");try{"sizing"==n[0]&&void 0!=t[parseInt(n[2])]&&(t[parseInt(n[2])].style.height=n[1]+"px")}catch(e){}}},s=function(t){e&&console.log(t);var i=t.origin.replace(/^https?\:\/\//i,"");if("lightwidget.com"==i||"instansive.com"==i){var n=t.data.split(":");e&&console.log(n);try{if("sizing_iid"==n[0]){var o=n[2];void 0!=document.getElementById(o)?document.getElementById(o).style.height=n[1]+"px":(o=o.replace("instansive","lightwidget"),void 0!=document.getElementById(o)&&(document.getElementById(o).style.height=n[1]+"px"))}}catch(d){e&&console.log(d)}}},g=function(){window.addEventListener?(window.addEventListener("message",c,!1),window.addEventListener("message",s,!1)):(window.attachEvent("onmessage",c),window.attachEvent("onmessage",s))};return g(),{refresh:function(){if(n)for(var e=0;i>e;e++)d(t[e],e)},reload:function(){g()},debug:function(t){return o(t)}}}()),window.lightwidget.refresh();
  
-
+/*Search desktop
+*************************************************/
+function searchDesktop() { 
+  $( ".has-mega-menu" ).hide();
+  $( "form.search-desktop" ).show("slide", { direction: "right" }, 200);
+  $( ".search-trigger-desktop" ).addClass('active');
+};
+function hideSearchDesktop() {  
+  $( "form.search-desktop" ).hide("slide", { direction: "right" }, 200);
+  $( ".has-mega-menu" ).show();
+  $( ".search-trigger-desktop" ).removeClass('active');
+};  
+$( ".search-trigger-desktop" ).on( "click", searchDesktop );
+$( ".search-desktop .overlay" ).on( "click",  hideSearchDesktop );
 
 /*Name Product
 *************************************************/
