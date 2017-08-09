3a58eb6454a9bc72e33fa3b04f21c03cf6546a2e
zp334966 <zanka000@hotmail.com>
2017-08-09 11:38:05 +0200

    Update combined2.js

--- combined2.js
+++ combined2.js
@@ -750,11 +750,11 @@ $(document).ready(function(){
   $('.exit').click(function() { 
     $('.site-layout').removeClass("advertisement-active");
     var items = document.getElementsByClassName("advertisement-desktop");
-    if (items != null) {
+    if (items != null && items != undefined) {
       items[0].style.display = "none";
     }
     var items = document.getElementsByClassName("advertisement-mobile");
-    if (items != null) {
+    if (items != null && items != undefined) {
       items[0].style.display = "none";  
     }
     try {
@@ -769,11 +769,11 @@ $(document).ready(function(){
   	if ('advertisement' in sessionStorage){
 		$('.site-layout').removeClass("advertisement-active");
 		var items = document.getElementsByClassName("advertisement-desktop");
-		if (items != null) {
+		if (items != null && items != undefined) {
 		  items[0].style.display = "none";
 		}
 		var items = document.getElementsByClassName("advertisement-mobile");
-		if (items != null) {
+		if (items != null && items != undefined) {
 		  items[0].style.display = "none"; 
 		}
   	}
