20802fd958206e4b31870c0446efad4a9cac41eb
zp334966 <zanka000@hotmail.com>
2017-08-10 13:50:34 +0200

    Update combined2.js

--- combined2.js
+++ combined2.js
@@ -1060,8 +1060,11 @@ $(document).ready(function(){ */
     };
     if ( $('#cart-dropdown').text().indexOf("Koszyk jest pusty") != -1 ){
       $('#cart-dropdown span').hide();
-      $('#cart-dropdown p').append("<img src='https://cdn.rawgit.com/zp334966/lamillou/f45ff1bf/assets/koszyk.svg'>");
-      $('#cart-dropdown p').append("<h5>W Twoim koszyku nie ma jeszcze żadnych produktów.</h5>");
+      if (! $( '#cart-dropdown .empty-cart' ).length ) {
+	  $('#cart-dropdown p').addClass('empty-cart');
+          $('#cart-dropdown p').append("<img src='https://cdn.rawgit.com/zp334966/lamillou/f45ff1bf/assets/koszyk.svg'>");
+          $('#cart-dropdown p').append("<h5>W Twoim koszyku nie ma jeszcze żadnych produktów.</h5>");
+      }
     };
   }; /*
   $(window).load(function() {
