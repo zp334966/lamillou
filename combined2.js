d52d657908c7cb5211103ffee6a717b7315c898c
joanna16 <joanna16@users.noreply.github.com>
2017-08-17 15:46:52 +0200

    Update combined2.js

--- combined2.js
+++ combined2.js
@@ -1191,7 +1191,7 @@ $(document).ready(function(){
 	/* : po Powiadom kiedy dostepny */
     	$('.footer-subscribe-widget.light-version.notify_prod p').append(":");
 	/* pusta przestrzen podglad obrazka */
-	if($('#exit-photo-view').css('display', 'none') == false) {
+	if($('#exit-photo-view').css('display', 'none') != true) {
         	$('.canvas-photo-view').hide();
     	}
 });
