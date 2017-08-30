4778c41238213b11977ffb09bc04cd2a142667cb
joanna16 <joanna16@users.noreply.github.com>
2017-08-30 11:54:47 +0200

    Update combined2.js

--- combined2.js
+++ combined2.js
@@ -1201,7 +1201,7 @@ $(document).ready(function(){
 	$('#cart-dropdown a').wrapAll( "<div class='scroll-menu' />");*/
 	/* Pod adresem nie ma produktu */
 	console.log('inside1');
-	if($('.container .page-heading h3').has(":contains('Pod tym adresem nie ma produktu')").length) {
+	if($('.container .page-heading h3:contains("Pod tym adresem nie ma produktu")').length) {
 		console.log('inside');
 		$(this).after('<div class="cms_user_profile"><div class="photo"><img src="https://cdn.rawgit.com/joanna16/lamillou/81b129e0/Group%208.svg"></div></div>');
 		$(this).addClass('no-product');
