a354c9c2c3ad8e78f34c0302792bbe5b16b4016d
zp334966 <zanka000@hotmail.com>
2017-08-31 14:48:59 +0200

    Update cart.js

--- cart.js
+++ cart.js
@@ -148,8 +148,8 @@ $( document ).ready(function() {
 
     
 
-    $('label[for="shop_order[delivery_option_id]"]:nth-last-of-type(2)').html('<strong>Kurier GLS</strong><br><span>na terenie Polski (14 zł)</span>');
-    $('label[for="shop_order[delivery_option_id]"]:last-of-type').html('<strong>Odbiór osobisty</strong><br><span>ul. Ruczaj 89, Warszawa (0 zł)</span>');
+    //$('label[for="shop_order[delivery_option_id]"]:nth-last-of-type(2)').html('<strong>Kurier GLS</strong><br><span>na terenie Polski (14 zł)</span>');
+    //$('label[for="shop_order[delivery_option_id]"]:last-of-type').html('<strong>Odbiór osobisty</strong><br><span>ul. Ruczaj 89, Warszawa (0 zł)</span>');
 
     //var $payu = $('label[for="shop_order[payment_kind]"]:nth-of-type(2)');
     var $payu = $('#payment_kind_selector input[value=platnosci_pl]').parent().next();
