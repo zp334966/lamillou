11b4e8e7c293d6a62c8724238768fe4327bade43
zp334966 <zanka000@hotmail.com>
2017-09-07 23:34:08 +0200

    Update validationForm.js

--- validationForm.js
+++ validationForm.js
@@ -12,11 +12,11 @@ function checkErrors() {
     var parentInput = $(this).next();
     var maybeError = $(this).next().next();
     if ( maybeError.length ) {
-      //if ( parentInput.hasClass('field_with_errors') ) {
+      if ( parentInput.hasClass('field_with_errors') ) {
         parentInput.addClass('field_with_errors');
         var error = mapForm[ $(this).text() ]; 
         parentInput.append("<div class='validation-error'>" + error + "</div>");
-     // }
+      }
     } else {
       parentInput.removeClass('field_with_errors');
       parentInput.find('.validation-error').remove();
