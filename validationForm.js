365da9ba3695a0b9ef1f7b0770f65d8ee7f5272f
zp334966 <zanka000@hotmail.com>
2017-09-07 23:32:43 +0200

    Update validationForm.js

--- validationForm.js
+++ validationForm.js
@@ -11,15 +11,15 @@ function checkErrors() {
   $('.lavina_form label').each(function() { 
     var parentInput = $(this).next();
     var maybeError = $(this).next().next();
-    if (maybeError.length) {
-      if ( parentInput.hasClass('field_with_errors') ) {
+    if ( maybeError.length ) {
+      //if ( parentInput.hasClass('field_with_errors') ) {
         parentInput.addClass('field_with_errors');
         var error = mapForm[ $(this).text() ]; 
         parentInput.append("<div class='validation-error'>" + error + "</div>");
-      }
+     // }
     } else {
       parentInput.removeClass('field_with_errors');
-      parentInput.find('validation-error').remove();
+      parentInput.find('.validation-error').remove();
     }
   });
 };
