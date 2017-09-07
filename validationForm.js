d457bf805eb1f1e4e28a1e5429b15ce9b0d66f01
zp334966 <zanka000@hotmail.com>
2017-09-07 23:30:18 +0200

    Update validationForm.js

--- validationForm.js
+++ validationForm.js
@@ -1,5 +1,5 @@
 var mapForm = { 
-  'Firma': 'Podaj nazwę formy', 
+  'Firma': 'Podaj nazwę firmy', 
   'Nazwisko i imię': 'Podaj nazwisko i imię', 
   'E-mail': 'Podaj e-mail',
   'Telefon' : 'Podaj telefon',
@@ -7,17 +7,19 @@ var mapForm = {
                 };
 
 
-function checkErrors() {
+function checkErrors() { 
   $('.lavina_form label').each(function() { 
     var parentInput = $(this).next();
     var maybeError = $(this).next().next();
     if (maybeError.length) {
-      parentInput.addClass('field_with_errors');
-      var error = mapForm[ $(this).text() ]; 
-      parentInput.append("<div class='validation-error'>" + error + "</div>");
+      if ( parentInput.hasClass('field_with_errors') ) {
+        parentInput.addClass('field_with_errors');
+        var error = mapForm[ $(this).text() ]; 
+        parentInput.append("<div class='validation-error'>" + error + "</div>");
+      }
     } else {
       parentInput.removeClass('field_with_errors');
-      parentInput.eq(1).remove();
+      parentInput.find('validation-error').remove();
     }
   });
 };
