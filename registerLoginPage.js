c539901fb59ed69580a22c18ca8bd6f196692b05
joanna16 <joanna16@users.noreply.github.com>
2017-09-15 12:49:32 +0200

    Update registerLoginPage.js

--- registerLoginPage.js
+++ registerLoginPage.js
@@ -143,12 +143,13 @@ function emptyCartElements() {
     $('.page').attr("id","empty-cart");
     $( '#empty-cart .page-heading.center h3' ).after("<img src='https://cdn.rawgit.com/zp334966/lamillou/f45ff1bf/assets/koszyk.svg'/>");
     $( '#empty-cart .page-heading img' ).after("<h5>W Twoim koszyku nie ma jeszcze żadnych produktów.</h5>");  
-    //$('#empty-cart br').remove();
+    $('#empty-cart br').remove();
+	/*
     $("#empty-cart br").each(function() {
 	    if(!$(this).has('div.extra-products')) {
 		    $(this).remove(); 
 	    } 
-    });
+    }); */
 };
 
 function emptyCart() {
