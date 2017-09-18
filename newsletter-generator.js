ebfcb737610eadd93e9a3293ea8976e4a974dd11
zp334966 <zanka000@hotmail.com>
2017-09-18 15:21:07 +0200

    Update newsletter-generator.js

--- newsletter-generator.js
+++ newsletter-generator.js
@@ -6,3 +6,42 @@ $('#zaladuj').on('click', function(){
   $('#newsletter-generated #main-title .h1').text(mainT);
   $('#newsletter-generated #main-subtitle .p').text(mainST);
 });
+
+$('#add-title button').on('click', function(){
+  var title = $('#title-form').val();
+  var html = genTitle(title);
+  addElement(html);
+});
+
+
+
+
+function addElement(elem) {
+  $('#newsletter-generated #news-content').append(elem);
+};
+
+function genTitle(title) {
+  var html = "<tr><td class='h1'>" + title + "</td></tr>";
+  return html;
+};
+
+function genSubtitle(subtitle) {
+  var html = "<tr><td class='p'>" + subtitle + "</td></tr>";
+  return html;
+};
+
+function genImgCategory(imgLink, link) {
+  var html = "<tr><td><a href='" + link + "'>" +
+      "<img class='img-category' src='" + imgLink + "'/>" +      
+      "</a></td></tr>";
+  return html;
+};
+
+function genButton(link) {
+  var html = "<tr><td><a class='btn-lam' href='" + link + "'>" +
+      "<table class='btn-lam' cellpadding='0' cellspacing='0' border='0'>" +
+      "<tr><td>Kup teraz</td></tr></table>" +
+      "</a></td></tr>";
+  return html;
+};
+
