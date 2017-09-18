af454bd5f3b8b66b3ff6503f026c267525393001
zp334966 <zanka000@hotmail.com>
2017-09-18 14:03:00 +0200

    Update newsletter-generator.js

--- newsletter-generator.js
+++ newsletter-generator.js
@@ -1,7 +1,7 @@
 $('#zaladuj').on('click', function(){
-  var mainImg = $('#main-image-form').value;
-  var mainT = $('#main-title-form').value;
-  var mainST = $('#main-subtitle-form').value;
+  var mainImg = $('#main-image-form').value();
+  var mainT = $('#main-title-form').value();
+  var mainST = $('#main-subtitle-form').value();
   alert(mainImg);
   
 });
