b6872f15371310e9c7bdcdc9832994c317ee99d9
joanna16 <joanna16@users.noreply.github.com>
2017-09-22 13:56:27 +0200

    Update combined2eng.js

--- combined2eng.js
+++ combined2eng.js
@@ -1094,7 +1094,8 @@ $(document).ready(function(){ */
       }
     };
     if ( $('#cart-dropdown').text().indexOf("Your cart is empty") == -1 && $( '#cart-dropdown .sum .total_cart_price' ).length ){
-	    var price = $('#cart-dropdown .sum .total_cart_price').text().replace(/[^0-9.]/g, "");
+	    var priceNode = $('#cart-dropdown .sum .total_cart_price');
+	    var price = priceNode.text().replace(/[^0-9.]/g, "");
 	    //console.log(price);
 	    $( '#cart-dropdown .totals .row div' ).each(function() {
 	    	if($(this).text().indexOf("Delivery") != -1 ) {
@@ -1105,6 +1106,8 @@ $(document).ready(function(){ */
 			}
 			else {
 				deliveryNode.text("€19.90");
+				var sum = 19.90 + price;
+				priceNode.text("€" + sum);
 			}
 		}
 	    });
