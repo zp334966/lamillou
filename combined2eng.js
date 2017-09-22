b20e30f17e36589934b4a2c569cbe046d813dda0
joanna16 <joanna16@users.noreply.github.com>
2017-09-22 14:57:08 +0200

    Update combined2eng.js

--- combined2eng.js
+++ combined2eng.js
@@ -986,6 +986,40 @@ function properTitle(word) {
   if (l > d) {return true; }
   else {return false;}
 };
+/*****************************************/
+/* Aktualizowanie ceny dostawy w koszyku */
+function updateCartDelivery() {
+     if (window.location.href.indexOf("/cart") > -1) {
+        if ( $('#cart-dropdown').text().indexOf("Your cart is empty") == -1 && $( '#cart-totals .sum .total_cart_price' ).length ){
+            var totalNode = $( '#cart-totals .sum .total_cart_price' );
+            var total = totalNode.text().replace(/[^0-9.]/g, "");
+            var priceNode;
+            $( '#cart-totals .totals .row div' ).each(function() {
+                if($(this).text().indexOf("Subtotal") != -1 ) {
+			priceNode = $(this).next();
+		}
+            });
+            var price = priceNode.text().replace(/[^0-9.]/g, "");
+            $( '#cart-totals .totals .row div' ).each(function() {
+                if($(this).text().indexOf("Delivery") != -1 ) {
+			var deliveryNode = $(this).next();
+			//var deliveryPrice = deliveryNode.text().replace(/[^0-9.]/g, "")
+			if (price >= 99.99) {
+			    deliveryNode.text("€0.00");
+			}
+			else {
+			    deliveryNode.text("€19.90");
+			    var sum = Number(19.90) + Number(price);
+			    if (total != sum) {
+				totalNode.text("€" + sum.toFixed(2));
+			    }
+			}
+		}
+            });
+        };
+    }
+}
+/*****************************************/
 /*slider nie dzialal wtedy
 $(document).ready(function(){ */
   if ($( '.product-extra .info h3 a' ).length ){
@@ -1109,19 +1143,20 @@ $(document).ready(function(){ */
 			//var deliveryPrice = deliveryNode.text().replace(/[^0-9.]/g, "")
 			if (price >= 99.99) {
 				deliveryNode.text("€0.00");
+				updateCartDelivery();
 			}
 			else {
 				deliveryNode.text("€19.90");
 				var sum = Number(19.90) + Number(price);
 				if (total != sum) {
 					totalNode.text("€" + sum.toFixed(2));
+					updateCartDelivery();
 				}
 			}
 		}
 	    });
     };
   };
-
 /*Unavaible products label
 *************************************************/ 
 $(document).ready(function(){
